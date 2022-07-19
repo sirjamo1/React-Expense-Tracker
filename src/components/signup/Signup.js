b8ccce1c18dba63f2099192a022adc28705747cf
khaled <khaledb.yahya@gmail.com>
2022-07-19 17:02:17 +0100

    initial commit

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -3,104 +3,123 @@ import "./Signup.css";
 import { useNavigate, useLocation } from "react-router-dom";
 import dollarInBirdCage from "../images/dollarInBirdCage.png";
 import {
-    collection,
-    getDocs,
-    addDoc,
-    updateDoc,
-    doc,
-    deleteDoc,
-    onSnapshot,
-    query,
-    setDoc,
-    where,
-    orderBy,
-    // serverTimestamp
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  doc,
+  deleteDoc,
+  onSnapshot,
+  query,
+  setDoc,
+  where,
+  orderBy,
+  // serverTimestamp
 } from "firebase/firestore";
 import {
-    createUserWithEmailAndPassword,
-    onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
 } from "firebase/auth";
 import { db } from "../../firebase-config";
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase-config";
+import GoogleButton from "react-google-button";
+import { useAuth } from "../../Auth";
+
 export function Signup() {
-    const [registerEmail, setRegisterEmail] = useState("");
-    const [registerPassword, setRegisterPassword] = useState("");
-    const [user, setUser] = useState({});
-    const navigate = useNavigate();
-    const location = useLocation();
-    const usersRef = collection(db, "users");
-    //const redirectPath = location.state?.path || "/";
-    onAuthStateChanged(auth, (currentUser) => {
-        setUser(currentUser);
-    });
-    const handleRegister = async () => {
-        try {
-            const user = await createUserWithEmailAndPassword(
-                auth,
-                registerEmail,
-                registerPassword
-            );
-            await addDoc(usersRef, {
-                email: auth.currentUser.email,
-                uid: auth.currentUser.uid,
-            });
-            sessionStorage.setItem("Auth Token", auth.currentUser.accessToken);
-            sessionStorage.setItem("uid", auth.currentUser.uid);
-            sessionStorage.setItem("email", auth.currentUser.email);
-            navigate("/dashboard");
-        } catch (error) {
-            console.log(error.message);
-        }
-    };
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [user, setUser] = useState({});
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+  const usersRef = collection(db, "users");
+  const { googleSignIn } = useAuth();
 
-    return (
-        <div className="main--container">
-            <div className="signup--container">
-                <h1>Sign up</h1>
-                <label>
-                    Email<span className="asterisk">*</span>
-                    <span className="instructions">
-                        (Must be a valid e-mail address)
-                    </span>
-                </label>
-                <input
-                    onChange={(event) => {
-                        setRegisterEmail(event.target.value);
-                    }}
-                    type="email"
-                    placeholder="Email"
-                    required
-                ></input>
-                <label>
-                    Password<span className="asterisk">*</span>
-                    <span className="instructions">
-                        (Minimum eight characters, at least one letter and one
-                        number)
-                    </span>
-                </label>
-                <input
-                    onChange={(event) => {
-                        setRegisterPassword(event.target.value);
-                    }}
-                    type="password"
-                    placeholder="password"
-                    pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
-                    required
-                ></input>
+  //const redirectPath = location.state?.path || "/";
+  onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+  });
+  const handleRegister = async () => {
+    try {
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+      );
+      await addDoc(usersRef, {
+        email: auth.currentUser.email,
+        uid: auth.currentUser.uid,
+      });
+      sessionStorage.setItem("Auth Token", auth.currentUser.accessToken);
+      sessionStorage.setItem("uid", auth.currentUser.uid);
+      sessionStorage.setItem("email", auth.currentUser.email);
+      navigate("/dashboard");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-                <button
-                    onClick={handleRegister}
-                    className="signup--submit"
-                    type="submit"
-                >
-                    Sign Up
-                </button>
-                <h4>
-                    <Link to="/"> Back to sign in page</Link>
-                </h4>
-            </div>
-            <img src={dollarInBirdCage} alt="dollar bill in a bird cage" />
-        </div>
-    );
+  const handleGoogleSignIn = async (e) => {
+    e.preventDefault();
+    try {
+      await googleSignIn();
+      navigate("/dashboard");
+    } catch (erorr) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div className="main--container">
+      <div className="signup--container">
+        <h1>Sign up</h1>
+        <label>
+          Email<span className="asterisk">*</span>
+          <span className="instructions">(Must be a valid e-mail address)</span>
+        </label>
+        <input
+          onChange={(event) => {
+            setRegisterEmail(event.target.value);
+          }}
+          type="email"
+          placeholder="Email"
+          required
+        ></input>
+        <label>
+          Password<span className="asterisk">*</span>
+          <span className="instructions">
+            (Minimum eight characters, at least one letter and one number)
+          </span>
+        </label>
+        <input
+          onChange={(event) => {
+            setRegisterPassword(event.target.value);
+          }}
+          type="password"
+          placeholder="password"
+          pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+          required
+        ></input>
+
+        <button
+          onClick={handleRegister}
+          className="signup--submit"
+          type="submit"
+        >
+          Sign Up
+        </button>
+        <GoogleButton
+          className="google-btn"
+          type="dark"
+          label="Sign Up with Google"
+          onClick={handleGoogleSignIn}
+        />
+        <h4>
+          <Link to="/"> Back to sign in page</Link>
+        </h4>
+      </div>
+      <img src={dollarInBirdCage} alt="dollar bill in a bird cage" />
+    </div>
+  );
 }
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,22 +2,28 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 import { getAuth } from "firebase/auth";
 //************ NOTE:*********************
-//need to put in your own firebase below*/ 
+//need to put in your own firebase below*/
 const firebaseConfig = {
-    apiKey: "AIzaSyDR90b1grij9mZjZkX_W1eEzk70pX-T1Ww",
-    authDomain: "react-expense-tracker-1a3d7.firebaseapp.com",
-    projectId: "react-expense-tracker-1a3d7",
-    storageBucket: "react-expense-tracker-1a3d7.appspot.com",
-    messagingSenderId: "307878146582",
-    appId: "1:307878146582:web:c88dcc8ed679d196577b37",
-    measurementId: "G-RZZS4N2DLM",
+  apiKey: "AIzaSyDdvyGwoGUodMKo2_Uawg_qPl3Fl7hJt6Y",
+
+  authDomain: "expense-tracker-dde20.firebaseapp.com",
+
+  projectId: "expense-tracker-dde20",
+
+  storageBucket: "expense-tracker-dde20.appspot.com",
+
+  messagingSenderId: "690891434595",
+
+  appId: "1:690891434595:web:1375281fc7cff15881e656",
+
+  measurementId: "G-BWZ9WZE95Q",
 };
+
 //************ NOTE:*********************
-//need to put in your own firebase above*/ 
+//need to put in your own firebase above*/
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-
 
 //James Firebase below
 // const firebaseConfig = {
