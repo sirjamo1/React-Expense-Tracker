a7311a58c02e461af098a792b6bec9d8aa26f899
Sirjamo1 <jam_how@hotmail.com>
2022-07-23 06:31:04 +1000

    Added back useEffect to auth.js

--- src/Auth.js
+++ src/Auth.js
@@ -4,16 +4,18 @@ import {
   GoogleAuthProvider,
   signInWithPopup,
 } from "firebase/auth";
-import { useState, createContext, useContext } from "react";
+import React, { useState, createContext, useContext } from "react";
 
 const AuthContext = createContext(null);
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const auth = getAuth();
-  onAuthStateChanged(auth, (user) => {
-    setUser(user);
-  });
-
+  React.useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
+          setUser(user);
+      });
+      return unsubscribe
+  },[]);
   function googleSignIn() {
     const googleAuthProvider = new GoogleAuthProvider();
     return signInWithPopup(auth, googleAuthProvider);
