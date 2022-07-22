3c14d11b47fd9f8c21ed614c735017dde219f1a2
Sirjamo1 <jam_how@hotmail.com>
2022-07-22 12:12:06 +1000

    Three most recent expenses displaying *needs css

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./Dashboard.css";
 import { db } from "../../firebase-config";
 import moment from "moment";
+import { Link } from "react-router-dom";
 import {
     collection,
     getDocs,
@@ -14,6 +15,7 @@ import {
     setDoc,
     where,
     orderBy,
+    limit,
     // serverTimestamp
 } from "firebase/firestore";
 
@@ -21,7 +23,28 @@ export const Dashboard = () => {
     const expenseDataRef = collection(db, "expenseData");
     const [expenseData, setExpenseData] = useState([]);
     const userUid = sessionStorage.getItem("uid");
+    const [threeRecent, setThreeRecent] = useState([]);
 
+    useEffect(() => {
+        const getThreeRecent = async () => {
+            const data = await getDocs(
+                query(expenseDataRef, orderBy("date", "desc"), limit(3))
+            );
+            const userData = data.docs.map((doc) => ({
+                ...doc.data(),
+                id: doc.id,
+            }));
+            const currentUserData = [];
+            for (let i = 0; i < userData.length; i++) {
+                if (userData[i].uid === userUid) {
+                    currentUserData.push(userData[i]);
+                }
+            }
+            setThreeRecent(currentUserData);
+        };
+        getThreeRecent();
+    }, []);
+    console.log(threeRecent);
     useEffect(() => {
         const getExpenseData = async () => {
             const data = await getDocs(expenseDataRef);
@@ -68,15 +91,49 @@ export const Dashboard = () => {
             }
         return total;
     };
+    console.log(expenseDataRef);
+
     const theDate = moment().format("YYYY-MM-DD");
     const total = getTotal();
     const monthly = getMonthly();
     const daily = getDaily();
+
+    const threeMostRecent = threeRecent.map((data) => (
+        <div className="row-data">
+            <div>
+                <p>{data.title}</p>
+            </div>
+            <div>
+                <p>{data.type}</p>
+            </div>
+            <div>
+                <p>${data.amount}</p>
+            </div>
+            <div>
+                <p>{data.date}</p>
+            </div>
+        </div>
+    ));
     return (
-        <div>
-            <h1>Total:{total}</h1>
-            <h1>Monthly Total:{monthly}</h1>
-            <h1>Daily Total:{daily}</h1>
+        <div><h1>Dashboard</h1>
+            <div className="three-totals">
+                <h1>Total:{total}</h1>
+                <h1>Monthly Total:{monthly}</h1>
+                <h1>Daily Total:{daily}</h1>
+            </div>
+            <div className="recent-expenses-header">
+                <h4>Recent Expenses</h4>
+                <button>
+                    <Link to="/expenses">View All</Link>
+                </button>
+            </div>
+            <div className="row-header">
+                <p>NAME/BUSINESS</p>
+                <p>TYPE</p>
+                <p>AMOUNT</p>
+                <p>DATE</p>
+            </div>
+            {threeMostRecent}
         </div>
     );
 };
