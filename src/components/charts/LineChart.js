e67270bcf603b5d7a743298f5cdc1ff8db673fd4
Sirjamo1 <jam_how@hotmail.com>
2022-07-27 14:09:14 +1000

    chart background color

--- src/components/charts/LineChart.js
+++ src/components/charts/LineChart.js
@@ -3,5 +3,5 @@ import { Line } from "react-chartjs-2";
 import { chart as chartjs } from "chart.js/auto";
 
 export function LineChart({ chartData }) {
-    return <Line data={chartData} />;
+    return <Line data={chartData} options={{ responsive: true }} />;
 }
