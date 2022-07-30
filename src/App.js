828d906b8eb8f5eace7b621a5b38c49bd65fb48c
khaled <khaledb.yahya@gmail.com>
2022-07-30 15:37:39 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Navbar } from "./components/navbar/Navbar.js";
 import { Signin } from "./components/signin/Signin";
-//import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { AuthProvider } from "./Auth";
 import { Signup } from "./components/signup/Signup.js";
 //import { Dashboard } from "./components/dashboard/Dashboard.js";
@@ -18,17 +18,17 @@ const LazyDashboard = React.lazy(() =>
 );
 
 function App() {
-  // const [user, setUser] = useState(null);
-  // const auth = getAuth();
-  // onAuthStateChanged(auth, (user) => {
-  //   setUser(user);
-  // });
+  const [user, setUser] = useState(null);
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    setUser(user);
+  });
   //See Navbar.js for details about hiding nav
   //If user == null the only page able to be viewed is Signin.js and Signup.js
   return (
     <AuthProvider>
       <main>
-        <Navbar />
+        {user && <Navbar />}
 
         <Routes>
           <Route path="/" element={<Signin />} />
